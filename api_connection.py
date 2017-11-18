e7932b6c57c95cf62f1b1b128ecfdd955532fc21
Nick Zambelli <stnvz01@moravian.edu>
2017-11-18 16:49:23 -0500

    Add some tests to the test class and implement them in api_connection.

--- api_connection.py
+++ api_connection.py
@@ -3,6 +3,7 @@ import spotipy.util as util
 import requests
 import os
 
+
 class Spotify_Connector:
 
 	def __init__(self):
@@ -15,18 +16,32 @@ class Spotify_Connector:
 		username = os.environ['SPOTIFY_USERNAME']
 		self.token = util.prompt_for_user_token(username,scope,client_id=client_id,client_secret=client_secret,redirect_uri=redirect_uri)
 
-	def artist_search(self, artist):
+	def get_artist_ids(self, artist):
 		url = 'https://api.spotify.com/v1/search?type=artist'
 		header = {'Authorization': 'Bearer ' + self.token}
 		r = requests.get(url, headers=header, params={'q': artist})
-		artist_list = {}
 		data = r.json()['artists']['items']
+		artist_list = {}
 		for num in range(len(data)):
 			artist_list[data[num]['name']] = data[num]['id']
 
+		return artist_list
+
+	def artist_search(self, artist):
+		artist_list = self.get_artist_ids(artist)
 
 		return list(artist_list.keys())
 
 	def related_artists(self, artist):
-		return artist
-		#url = 'https://api.spotify.com/v1/artists/32vWCbZh0xZ4o9gkz4PsEU'
+		artists = self.get_artist_ids(artist)
+
+		url = 'https://api.spotify.com/v1/artists/{}/related-artists'.format(artists.get(artist))
+		header = {'Authorization': 'Bearer ' + self.token}
+		r = requests.get(url, headers=header)
+		r.raise_for_status()
+		json_list = r.json()['artists']
+		related_artists = []
+		for num in range(len(json_list)):
+			related_artists.append(json_list[num]['name'])
+		return related_artists
+
--- test_spotify_connector.py
+++ test_spotify_connector.py
@@ -4,8 +4,14 @@ from api_connection import Spotify_Connector
 
 class TestSpotifyConnector(TestCase):
 
-	def test_artist_search(self):
+	def test_artist_search_no_matches(self):
 		sc = Spotify_Connector()
 		artists = sc.artist_search('reaknsvl')
 		self.assertEqual(0, len(artists))
 		self.assertEqual([], artists)
+
+	def test_related_artists_no_matches(self):
+		sc = Spotify_Connector()
+		self.assertRaises(Exception, sc.related_artists, 'acsnjr')
+
+
