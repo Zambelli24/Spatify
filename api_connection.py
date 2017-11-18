a3c7606c00ee34d0b2312ae7c0f7300588584a15
Nick Zambelli <stnvz01@moravian.edu>
2017-11-18 16:17:35 -0500

    Make the file into a class with methods, and begin implementing the artist_search method

--- api_connection.py
+++ api_connection.py
@@ -3,28 +3,30 @@ import spotipy.util as util
 import requests
 import os
 
-client_id = os.environ['SPOTIFY_CLIENT_ID']
-client_secret = os.environ['SPOTIFY_CLIENT_SECRET']
+class Spotify_Connector:
 
-authorization_base_url = "https://accounts.spotify.com/authorize"
-token_url = 'https://accounts.spotify.com/api/token'
-#redirect_uri = 'http://localhost:8888/callback'
-redirect_uri = 'http://localhost:5000/callback'
+	def __init__(self):
+		client_id = os.environ['SPOTIFY_CLIENT_ID']
+		client_secret = os.environ['SPOTIFY_CLIENT_SECRET']
+		authorization_base_url = "https://accounts.spotify.com/authorize"
+		token_url = 'https://accounts.spotify.com/api/token'
+		redirect_uri = 'http://localhost:8888/callback'
+		scope = 'user-library-read'
+		username = os.environ['SPOTIFY_USERNAME']
+		self.token = util.prompt_for_user_token(username,scope,client_id=client_id,client_secret=client_secret,redirect_uri=redirect_uri)
 
-scope = 'user-library-read'
-username = os.environ['SPOTIFY_USERNAME']
+	def artist_search(self, artist):
+		url = 'https://api.spotify.com/v1/search?type=artist'
+		header = {'Authorization': 'Bearer ' + self.token}
+		r = requests.get(url, headers=header, params={'q': artist})
+		artist_list = {}
+		data = r.json()['artists']['items']
+		for num in range(len(data)):
+			artist_list[data[num]['name']] = data[num]['id']
 
-token = util.prompt_for_user_token(username,scope,client_id=client_id,client_secret=client_secret,redirect_uri=redirect_uri)
-print(token)
 
-#url = 'https://api.spotify.com/v1/artists/32vWCbZh0xZ4o9gkz4PsEU'
+		return list(artist_list.keys())
 
-url = 'https://api.spotify.com/v1/search?type=artist'
-
-header = {'Authorization': 'Bearer ' + token}
-
-r = requests.get(url, headers=header, params={'q': 'Dolly Parton'})
-
-print(r.json()['artists']['items'][0]['name'])
-
-print(r.json()['artists']['items'][0]['id'])
+	def related_artists(self, artist):
+		return artist
+		#url = 'https://api.spotify.com/v1/artists/32vWCbZh0xZ4o9gkz4PsEU'
