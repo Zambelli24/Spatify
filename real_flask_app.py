4d0f1e570912aea7c1b28918a2546ffede7585b3
John Spirk <stjfs03@moravian.edu>
2017-11-20 11:29:31 -0500

    Removed duplicate code in response messages in flask app

--- real_flask_app.py
+++ real_flask_app.py
@@ -2,15 +2,21 @@ from flask import Flask, request, Response
 from api_connection import Spotify_Connector
 
 sc = Spotify_Connector()
+name_unfilled_response = "Name parameter not filled. Must provide '?artist=[artist name]' following query."
 
 app = Flask(__name__)
+
+
+@app.route('/')
+def homepage():
+    return 'welcome to the api'
 
 
 @app.route('/search_artist')
 def artist_search():
     artist = request.args.get('artist')
     if artist is None:
-        return Response("Name parameter not fulfilled.", status=400)
+        return Response("{}".format(name_unfilled_response), status=400)
 
     return str(sc.artist_search(artist))
 
@@ -19,7 +25,9 @@ def artist_search():
 def track_search():
     artist = request.args.get('artist')
     if artist is None:
-        return Response("Name parameter not fulfilled.", status=400)
+        return Response("{}".format(name_unfilled_response),
+                        status=400)
+
     return str(sc.track_search(artist))
 
 
@@ -27,7 +35,8 @@ def track_search():
 def related_artists():
     artist = request.args.get('artist')
     if artist is None:
-        return Response("Name parameter not fulfilled.", status=400)
+        return Response("{}".format(name_unfilled_response),
+                        status=400)
 
     return str(sc.related_artists(artist))
 
