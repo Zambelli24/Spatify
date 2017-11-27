d0c6e1f07c46b14fa79c31866d0a8668af396b66
Nick Zambelli <stnvz01@moravian.edu>
2017-11-27 10:29:14 -0500

    Update the tests and add a new one for the new spotify_connection file.

--- test_spotify_connector.py
+++ test_spotify_connector.py
@@ -1,31 +1,31 @@
 from unittest import TestCase
 
 from api_connection import Spotify_Connector
+from spotify_connection import get_all_tracks, artist_search, related_artists
+
 
 class TestSpotifyConnector(TestCase):
 
 	def test_artist_search_no_matches(self):
-		sc = Spotify_Connector()
-		artists = sc.artist_search('reaknsvl')
+		artists = artist_search('reaknsvl')
 		self.assertEqual(0, len(artists))
 		self.assertEqual([], artists)
 
 	def test_related_artists_no_matches(self):
-		sc = Spotify_Connector()
-		self.assertRaises(Exception, sc.related_artists, 'acsnjr')
+		self.assertRaises(Exception, related_artists, 'acsnjr')
 
 	def test_artist_search_small_list(self):
-		sc = Spotify_Connector()
-		artists = sc.artist_search('cupcakke')
+		artists = artist_search('cupcakke')
 		self.assertEqual(2, len(artists))
 		self.assertEqual(['cupcakKe', 'Cupcakke'], artists)
 
 	def test_related_artists_small_list(self):
-		sc = Spotify_Connector()
-		artists = sc.related_artists('Katy Perry')
+		artists = related_artists('Katy Perry')
 		self.assertEqual(20, len(artists))
 
 	def test_track_search_artist_with_few_songs(self):
-		sc = Spotify_Connector()
-		tracks = sc.track_search('Cupcakke')
+		tracks = get_all_tracks('Cupcakke')
 		self.assertEqual(1, len(tracks))
+
+	def test_track_search_not_real_artist(self):
+		self.assertRaises(Exception, get_all_tracks, 'neibvh')
